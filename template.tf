05e20d7f3d81dad4d9d5e6dd3fbbd5d3f1a7d5a4
Adam Cavaliere <adam.cavaliere@gmail.com>
2018-04-13 12:50:44 -0500

    added vault / consul cluster sizes

--- template.tf
+++ template.tf
@@ -2,7 +2,7 @@ data "template_file" "consul" {
   template = "${file("${path.module}/init-consul.sh")}"
 
   vars = {
-    cluster_size     = "${var.cluster_size}"
+    cluster_size     = "${var.consul_cluster_size}"
     environment_name = "${var.environment_name}"
     local_region     = "${var.region}"
   }
@@ -12,7 +12,7 @@ data "template_file" "vault" {
   template = "${file("${path.module}/init-vault.sh")}"
 
   vars = {
-    cluster_size     = "${var.cluster_size}"
+    cluster_size     = "${var.vault_cluster_size}"
     environment_name = "${var.environment_name}"
     local_region     = "${var.region}"
   }

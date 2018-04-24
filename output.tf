a0e457157b586bc38c35fbd008362c09b84e8e43
Adam Cavaliere <adam.cavaliere@gmail.com>
2018-04-23 22:14:50 -0500

    add data

--- output.tf
+++ output.tf
@@ -9,3 +9,23 @@ output "db_port" {
 output "vault_url" {
   value = "${aws_route53_record.www.name}"
 }
+
+data "aws_instances" "vault_servers" {
+  instance_tags {
+    Name = "Vault Server"
+  }
+}
+
+resource "vault_generic_secret" "vault_output" {
+path = "secret/cluster_details"
+
+data_json = <<EOT
+{
+  "cluster_address": "${aws_route53_record.www.name}"
+  "db_address": "${aws_db_instance.example.address}"
+  "vault_server": "${element(data.aws_instances.vault_servers.public_ips, 0)}"
+}
+EOT
+}
+
+}
